d2dce82a24744c752ae77a9436377286cc7f0fb5
Jose Luis <joseluismartinez1024@gmail.com>
2021-12-24 12:18:05 -0600

    Formato del DataGridView.

--- Promediador/Form1.cs
+++ Promediador/Form1.cs
@@ -40,7 +40,44 @@ namespace Promediador
         {
             try
             {
+                // Limpiar Grid
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+                // Obtener la cantidad de alumnos
+                int cantAl = int.Parse(txtCantAl.Text);
 
+                // Obtener el número de calificaciones
+                int canCal = int.Parse(txtCalPorAl.Text);
+
+                // Obtener el promedio aprobatorio
+                double prom = double.Parse(txtProm.Text);
+
+                // Indicar número de columnas en el GRID, NO LISTA, CALIFICACIONES, PROMEDIO Y ESTADO
+                dataGridView1.ColumnCount = 1 + canCal + 2;
+
+                // Agregar nombre a la primer columna
+                dataGridView1.Columns[0].Name = "No. Lista";
+
+
+                // Ciclo FOR para agregar las columnas necesarias de la cantidad de calificaciones
+                // Ya se agreago una columna en la posición 0 : No.Lista
+
+                // Se debe iniciar el ciclo a partir de la posición 1: para ahí comenzar a poner las
+                // columas de las calificaciones, son obtenidas de la cantidad de calificaciones.
+                for (int col = 1; col <= canCal; col++)
+                {
+                    // Agregar el nombre de cada columna de calificaciones, 1,2,3...
+                    dataGridView1.Columns[col].Name = "Calificación " + col;
+
+                    // Verificar cuando se terminen de agregar las columnas de calificaciones
+                    // Para poder agregar las columnas de PROMEDIO y ESTADO
+                    // Cuando estos valores sean iguales, las calificaciones ya se agregaron.
+                    if (col == canCal)
+                    {
+                        dataGridView1.Columns[col + 1].Name = "Promedio";
+                        dataGridView1.Columns[col + 2].Name = "Estado";
+                    }
+                }
             }
             catch (Exception)
             {
