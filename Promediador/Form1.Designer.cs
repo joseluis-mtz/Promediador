0496992f1e82bb11329d01f14fd92bf0b1106a3e
Jose Luis <joseluismartinez1024@gmail.com>
2021-12-24 12:10:20 -0600

    Crear interfaz de usuario.

--- Promediador/Form1.Designer.cs
+++ Promediador/Form1.Designer.cs
@@ -28,12 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnProm = new System.Windows.Forms.Button();
+            this.txtProm = new System.Windows.Forms.TextBox();
+            this.txtCalPorAl = new System.Windows.Forms.TextBox();
+            this.txtCantAl = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnProm);
+            this.groupBox1.Controls.Add(this.txtProm);
+            this.groupBox1.Controls.Add(this.txtCalPorAl);
+            this.groupBox1.Controls.Add(this.txtCantAl);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(912, 240);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Generales";
+            // 
+            // btnProm
+            // 
+            this.btnProm.AutoSize = true;
+            this.btnProm.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnProm.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnProm.Location = new System.Drawing.Point(17, 171);
+            this.btnProm.Name = "btnProm";
+            this.btnProm.Size = new System.Drawing.Size(165, 48);
+            this.btnProm.TabIndex = 12;
+            this.btnProm.Text = "Ingresar Datos";
+            this.btnProm.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProm.UseVisualStyleBackColor = true;
+            // 
+            // txtProm
+            // 
+            this.txtProm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProm.Location = new System.Drawing.Point(310, 122);
+            this.txtProm.Name = "txtProm";
+            this.txtProm.Size = new System.Drawing.Size(100, 26);
+            this.txtProm.TabIndex = 11;
+            // 
+            // txtCalPorAl
+            // 
+            this.txtCalPorAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCalPorAl.Location = new System.Drawing.Point(485, 77);
+            this.txtCalPorAl.Name = "txtCalPorAl";
+            this.txtCalPorAl.Size = new System.Drawing.Size(100, 26);
+            this.txtCalPorAl.TabIndex = 10;
+            // 
+            // txtCantAl
+            // 
+            this.txtCantAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantAl.Location = new System.Drawing.Point(329, 38);
+            this.txtCantAl.Name = "txtCantAl";
+            this.txtCantAl.Size = new System.Drawing.Size(100, 26);
+            this.txtCantAl.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(291, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Escriba cuál es el promedio aprobatorio:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(466, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ingrese la cantidad de calificaciones por cada alumno a capturar:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ingrese la cantidad de alumnos a capturar:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 258);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(915, 350);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos Especificos";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(909, 325);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(929, 608);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Promediador";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnProm;
+        private System.Windows.Forms.TextBox txtProm;
+        private System.Windows.Forms.TextBox txtCalPorAl;
+        private System.Windows.Forms.TextBox txtCantAl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Promediador/Form1.cs
+++ Promediador/Form1.cs
@@ -15,5 +15,10 @@ namespace Promediador
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
